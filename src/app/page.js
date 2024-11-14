d8d99eb30cae3bd569eb8dd82bea01cc0f7bb322
EAGLE1309 <vighneshbangar57@gmail.com>
2024-11-14 11:25:47 +0530

    Preparing for testing phase

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
-import AuthScreen from "@/features/auth/components/auth-screen";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
-  return <AuthScreen />;
+  return (
+    <section className="h-screen flex items-center justify-center bg-black">
+      <Card>
+        <CardHeader>
+          <CardTitle>Testing features - alpha demo v1.1</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col justify-between gap-y-2.5">
+          <Button asChild>
+            <Link href="/auth">Login / Register</Link>
+          </Button>
+          <Button className="opacity-50 pointer-events-none !cursor-no-drop">
+            Chats
+          </Button>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
--- src/features/auth/components/auth-screen.jsx
+++ src/features/auth/components/auth-screen.jsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 
 import SignInCard from "./sign-in-card";
 import SignUpCard from "./sign-up-card";
+import { Button } from "@/components/ui/button";
+import { RiArrowLeftLine } from "react-icons/ri";
+import Link from "next/link";
 
 const AuthScreen = () => {
   const [state, setState] = useState("signIn");
 
   return (
-    <div className="h-full flex items-center justify-center bg-black">
+    <div className="h-full flex flex-col gap-y-5 items-center justify-center bg-black">
       <div className="md:h-auto md:w-[420px]">
         {state === "signIn" ? (
           <SignInCard setState={setState} />
@@ -17,6 +20,12 @@ const AuthScreen = () => {
           <SignUpCard setState={setState} />
         ) : null}
       </div>
+      <Button asChild className="bg-black border-[1px] border-white">
+        <Link href="/">
+          Go Back
+          <RiArrowLeftLine />
+        </Link>
+      </Button>
     </div>
   );
 };
