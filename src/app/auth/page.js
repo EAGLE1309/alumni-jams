97e83fcd632375c8994e2b46437dc694a873805c
EAGLE1309 <vighneshbangar57@gmail.com>
2024-11-17 00:36:35 +0530

    Fixed minor issues

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,5 +1,10 @@
 import AuthScreen from "@/components/auth/auth-screen";
+import { Suspense } from "react";
 
 export default function AuthPage() {
-  return <AuthScreen />;
+  return (
+    <Suspense>
+      <AuthScreen />;
+    </Suspense>
+  );
 }
